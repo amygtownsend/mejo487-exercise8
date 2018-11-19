6b620426036113e2d639610b8ba34dab995a501c
UNC Support <uncsupport@ITSs-MacBook-Pro.local>
2018-11-19 03:03:17 -0500

    add geocoding api

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,29 @@ $(function(){
       data: data,
       success: function(data){
         console.log(data);
+        var latitude = data.iss_position.latitude;
+        var longitude = data.iss_position.longitude;
+        var url2 = 'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=' + latitude + '&lon=' + longitude;
+        var data2 = [];
+        var html = '';
+
+        $.ajax({
+          type: 'GET',
+          url: url2,
+          dataType: 'json',
+          async: true,
+          data: data2,
+          success: function(data2){
+            console.log(data2);
+            if (data2.error) {
+              html += 'The space station is currently over an ocean.';
+              $('#results').html(html);
+            } else {
+              html += 'The space station is currently over ' + data2.address.city + ', ' + data2.address.country;
+              $('#results').html(html);
+            }
+          }
+        });
       }
     });
   }
